c469dee0cc8df7aae709bd769313bd63ce6a4780
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-15 18:49:07 -0300

    [ :sparkles: ] | create: fun - get_id (app::main)

--- rust_api/src/main.rs
+++ rust_api/src/main.rs
@@ -46,3 +46,17 @@ fn setup_database() -> Result<(), PostgresError> {
     )?;
     Ok(())
 }
+
+fn get_user_request_body(request: &str) -> Result<User, serde_json::Error> {
+    serde_json::from_str(request.split("\r\n\r\n").last().unwrap_or_default())
+}
+
+fn get_id(request: &str) -> &str {
+    request
+        .split("/")
+        .nth(2)
+        .unwrap_or_default()
+        .split_whitespace()
+        .next()
+        .unwrap_or_default()
+}
