2505f4a6db06783dd1e05375e7bc2d83f811aaf3
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-15 11:44:03 -0300

    [ :sparkles: ] | create: model - User (module::core)

--- rust_api/src/main.rs
+++ rust_api/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate serde_derive;
+
+// Model
+#[derive(Serialize, Deserialize)]
+struct User {
+    id: Option<i32>,
+    name: String,
+    email: String,
 }
+
+fn main() {}
